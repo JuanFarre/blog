dac4406b928114d9043382acc71424a8ac245b69
JuanPelu1999 <juanfarre99@gmail.com>
2024-07-16 23:31:14 -0300

    add OAuth2 google

--- src/main/java/pelusadev/ProyectoBlog/security/config/SecurityConfig.java
+++ src/main/java/pelusadev/ProyectoBlog/security/config/SecurityConfig.java
@@ -15,9 +15,11 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import pelusadev.ProyectoBlog.security.config.filter.JwtTokenValidator;
+import pelusadev.ProyectoBlog.utils.CustomAuthenticationSuccessHandler;
 import pelusadev.ProyectoBlog.utils.JwtUtils;
 
 @Configuration
@@ -28,40 +30,54 @@ public class SecurityConfig {
     @Autowired
     private JwtUtils jwtUtils;
 
+    @Autowired
+    private CustomAuthenticationSuccessHandler customAuthenticationSuccessHandler; // Inyecta tu handler personalizado aquí
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity
+        httpSecurity
                 .csrf(csrf -> csrf.disable())
                 .httpBasic(Customizer.withDefaults())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .addFilterBefore(new JwtTokenValidator(jwtUtils), BasicAuthenticationFilter.class)
-                .build();
+                .authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests
+                                .requestMatchers("/", "/auth/login", "/error**", "/oauth2/**").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .oauth2Login(oauth2Login ->
+                        oauth2Login
+                                .loginPage("/auth/login")
+                                .userInfoEndpoint(userInfoEndpoint ->
+                                        userInfoEndpoint.oidcUserService(oidcUserService())
+                                )
+                                .successHandler(customAuthenticationSuccessHandler) // Utiliza el handler personalizado aquí
+                )
+                .addFilterBefore(new JwtTokenValidator(jwtUtils), UsernamePasswordAuthenticationFilter.class);
+
+        return httpSecurity.build();
     }
 
+    @Bean
+    public OidcUserService oidcUserService() {
+        return new OidcUserService();
+    }
 
-    //creamos authentication manager
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
     }
-    //creamos authentication provider
-    //Agregamos el user Details Service como parámetro
+
     @Bean
     public AuthenticationProvider authenticationProvider(UserDetailsService userDetailsService){
         DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
         provider.setPasswordEncoder(passwordEncoder());
-        //sacamos el anterior, el lógico y agregamos el nuevo
         provider.setUserDetailsService(userDetailsService);
-
 
         return provider;
     }
 
-    //password encoder
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
-
-
 }
---
+++ src/main/java/pelusadev/ProyectoBlog/utils/CustomAuthenticationSuccessHandler.java
@@ -0,0 +1,20 @@
+package pelusadev.ProyectoBlog.utils;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+@Component
+public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+                                        Authentication authentication) throws IOException, ServletException {
+        // Redirigir a una URL específica después del login exitoso
+        response.sendRedirect("https://www.instagram.com/farre.juan/");
+    }
+}
